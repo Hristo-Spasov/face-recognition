d6555d3905da76149be62735b914e8710446e511
Trickkyyy <vandark5@gmail.com>
2023-02-11 05:10:08 +0200

    app progression

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,69 @@ import React,{ Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from "./components/Logo/Logo";
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import ParticlesBg from 'particles-bg'
+import ParticlesBg from 'particles-bg';
 import './App.css';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }  
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    const PAT = '76d9f0ea2bf64630b23f756b4f0b84cc';
+    const USER_ID = 'tricky';       
+    const APP_ID = 'face-detection';
+    const IMAGE_URL = this.state.input;
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {  
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+    });
+
+      const requestOptions = {
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Authorization': 'Key ' +  PAT 
+          },
+          body: raw
+      };
+
+
+
+      fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(error => console.log('error', error));
+  
+
+  }
+
   render() {
     return (
       <div className='App'>
@@ -15,8 +72,8 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* {<FaceRecognition />} */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition  imageUrl={this.state.imageUrl} />
       </div>
     )
   }
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css"
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p className="f3 tc">
@@ -9,9 +9,9 @@ const ImageLinkForm = () => {
             </p>
             <div className="center form flex-column items-center shadow-2">
 
-                <input className="f4 fl w-70 pa2 br4 bn ma3 outline-transparent" type="text" placeholder={"Put your URL here"}/>
+                <input className="f4 fl w-70 pa2 br4 bn ma3 outline-transparent" type="text" placeholder={"Put your URL here"} onChange={onInputChange} />
                 
-                <button className=" grow f4 link ph3 pv2  white bg-light-purple bn br2 ma2">
+                <button className=" grow f4 link ph3 pv2  white bg-light-purple bn br2 ma2" onClick={onButtonSubmit}>
                     {"Detect"}
                 </button> 
 
@@ -21,3 +21,8 @@ const ImageLinkForm = () => {
 }
 
 export default ImageLinkForm
+
+
+
+
+
