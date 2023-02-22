1c7102c8a8c17f0398f44a22dc844bf1fcfdcd92
Trickkyyy <vandark5@gmail.com>
2023-02-22 22:16:53 +0200

    v.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    fetch('http://localhost:3000/imageUrl',{
+    fetch('https://face-recognition-backend-me54.onrender.com/imageUrl',{
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body:JSON.stringify({
@@ -76,7 +76,7 @@ class App extends Component {
     .then(response => response.json())
       .then(response => { 
         if (response) {
-          fetch('http://localhost:3000/imagescore', {
+          fetch('https://face-recognition-backend-me54.onrender.com/imagescore', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
